e29c83d21522e3ee85e53abc126a609a44b09d1d
James Forsythe <forsytjr@gmail.com>
2016-03-05 11:21:04 -0500

    chanbes

---
+++ src/com/concordrobotics/stronghold/CustomUltrasonic.java
@@ -0,0 +1,12 @@
+package com.concordrobotics.stronghold;
+
+import edu.wpi.first.wpilibj.Ultrasonic;
+
+public class CustomUltrasonic extends Ultrasonic {
+
+	public CustomUltrasonic(int pingChannel, int echoChannel) {
+		super(pingChannel, echoChannel);
+		// TODO Auto-generated constructor stub
+	}
+
+}
